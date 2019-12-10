7fdfc25d580f922153d9045adb2b35c40acb6b46
woohyeon Ahn <europeluv@gmail.com>
2019-12-10 21:28:16 +0900

    first commit, ignore prev commits

---
+++ VecClass/MyVector/mymain.cpp
@@ -0,0 +1,15 @@
+// mymain.cpp : This file contains the 'main' function.
+
+#include <iostream>
+#include <memory>
+#include <vector>
+#include "Vec.h"
+#include "Vec.cpp"
+
+
+
+int main()
+{
+	
+	return 0;
+}
